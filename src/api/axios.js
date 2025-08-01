ffb439dff1d14924cc4b5d94be5e11333b8b9dfa
yashalsaleem <yashalsaleem116@gmail.com>
2025-08-01 14:41:16 +0500

    API Integration

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,5 +3,13 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const addTaskAPI = (task) => {
-  return axios.post(`${BASE_URL}/tasks/add/`, task);
+  return axios.post(`${BASE_URL}/tasks/add/`, task)
+    .then(res => {
+      console.log('Task added:', res.data);
+      return res.data;
+    })
+    .catch(error => {
+      console.error('Error adding task:', error);
+      throw error;
+    });
 };
